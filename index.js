b2313a6d9499f3356da88c30f81fd473fc0ca0aa
Giorgi Gvintidze <giorgininidze200@gmail.com>
2023-10-23 02:47:14 +0400

    deds

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBmgIRJw0Gh3ihy_hE2D9WpY3Xx5VbrmNU",
-  authDomain: "movies-api-52dec.firebaseapp.com",
-  projectId: "movies-api-52dec",
-  storageBucket: "movies-api-52dec.appspot.com",
-  messagingSenderId: "552233901863",
-  appId: "1:552233901863:web:680360d57cf0d2df069302",
-  measurementId: "G-W40J023F1Y",
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBmgIRJw0G1h3ihy_hE2D9WpY3Xx5VbrmNU",
+    authDomain: "movies-api-52dec.firebaseapp.com",
+    projectId: "movies-api-52dec",
+    storageBucket: "movies-api-52dec.appspot.com",
+    messagingSenderId: "552233901863",
+    appId: "1:552233901863:web:680360d57cf0d2df069302",
+    measurementId: "G-W40J023F1Y",
+  });
 
-const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
 
-console.log(`sss`);
+  console.log(`sss`);
 
-const login = () => {
-  const email = document.querySelector(".email-input").value; // Use querySelector instead of getElementsByClassName
-  const password = document.querySelector(".password-input").value; // Use querySelector instead of getElementsByClassName
+  const login = () => {
+    const email = document.querySelector(".email-input").value;
+    const password = document.querySelector(".password-input").value;
 
-  auth
-    .signInWithEmailAndPassword(email, password)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-const btn = document.querySelector(".form-btn"); // Use querySelector instead of getElementsByClassName
+  const btn = document.querySelector(".form-btn");
 
-btn.addEventListener("click", () => {
-  console.log(`inside`);
-  login();
+  btn.addEventListener("click", () => {
+    console.log(`inside`);
+    login();
+  });
 });
