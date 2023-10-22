67a78a519579d25e10ca1f12035a4fa5e5d5af26
Giorgi Gvintidze <giorgininidze200@gmail.com>
2023-10-23 02:31:48 +0400

    updaet 1

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-window.addEventListener("DOMContentLoaded", () => {
-  var currentDomain = window.location.hostname;
-  console.log(currentDomain);
-  const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBmgIRJw0Gh3ihy_hE2D9WpY3Xx5VbrmNU",
-    authDomain: "movies-api-52dec.firebaseapp.com",
-    projectId: "movies-api-52dec",
-    storageBucket: "movies-api-52dec.appspot.com",
-    messagingSenderId: "552233901863",
-    appId: "1:552233901863:web:680360d57cf0d2df069302",
-    measurementId: "G-W40J023F1Y",
-  });
-  const db = firebaseApp.firestore();
-  const auth = firebaseApp.auth();
+console.log(currentDomain);
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBmgIRJw0Gh3ihy_hE2D9WpY3Xx5VbrmNU",
+  authDomain: "movies-api-52dec.firebaseapp.com",
+  projectId: "movies-api-52dec",
+  storageBucket: "movies-api-52dec.appspot.com",
+  messagingSenderId: "552233901863",
+  appId: "1:552233901863:web:680360d57cf0d2df069302",
+  measurementId: "G-W40J023F1Y",
 });
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+const login = () => {
+  const email = document.getElementsByClassName("email-input").value;
+
+  const password = document.getElementsByClassName("password-input").value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
