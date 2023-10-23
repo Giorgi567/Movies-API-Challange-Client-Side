c805893e1615aa4f8848259b8c763183fee155ff
Giorgi Gvintidze <giorgininidze200@gmail.com>
2023-10-23 17:33:37 +0400

    d

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBmgIRJw0Gh3ihy_hE2D9WpY3Xx5VbrmNU",
+  authDomain: "movies-api-52dec.firebaseapp.com",
+  projectId: "movies-api-52dec",
+  storageBucket: "movies-api-52dec.appspot.com",
+  messagingSenderId: "552233901863",
+  appId: "1:552233901863:web:680360d57cf0d2df069302",
+  measurementId: "G-W40J023F1Y",
+});
 document.addEventListener("DOMContentLoaded", function () {
-  const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBmgIRJw0Gh3ihy_hE2d9WpY3Xx5VbrmNU",
-    authDomain: "movies-api-52dec.firebaseapp.com",
-    projectId: "movies-api-52dec",
-    storageBucket: "movies-api-52dec.appspot.com",
-    messagingSenderId: "552233901863",
-    appId: "1:552233901863:web:680360d57cf0d2df069302",
-    measurementId: "G-W40J023F1Y",
-  });
-
   const db = firebaseApp.firestore();
   const auth = firebaseApp.auth();
-
-  console.log(`sss`);
 
   const login = () => {
     const email = document.querySelector(".email-input").value;
